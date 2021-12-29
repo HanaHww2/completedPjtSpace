77a29768dd000bf2354753dd4fe4707987a71611
HanaHww2 <hanahww2@gmail.com>
2021-12-29 09:49:23 +0900

    remove: move structure/ to seperate repo

--- algorithm/src/me/study/structure/HashI.java
+++
@@ -1,4 +0,0 @@
-package me.study.structure;
-
-public interface HashI<K, V> extends Iterable<K>{
-}
--- algorithm/src/me/study/structure/HashListI.java
+++
@@ -1,6 +0,0 @@
-package me.study.structure;
-
-public interface HashListI<E> extends Iterable<E>{
-    void addFirst(E e);
-    E remove(E e);
-}
